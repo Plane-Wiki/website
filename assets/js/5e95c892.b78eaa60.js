5b1f909bf9a7ac0df7c247f0d752f74bd656fbb3
LoryGames <briciola.mc@gmail.com>
2024-10-18 20:57:41 +0200

    Updates

---
+++ assets/js/5e95c892.b78eaa60.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkplanewiki=self.webpackChunkplanewiki||[]).push([[647],{7121:(e,s,r)=>{r.r(s),r.d(s,{default:()=>p});r(6540);var a=r(4164),n=r(1213),u=r(7559),c=r(2831),i=r(781),l=r(4848);function p(e){return(0,l.jsx)(n.e3,{className:(0,a.A)(u.G.wrapper.docsPages),children:(0,l.jsx)(i.A,{children:(0,c.v)(e.route.routes)})})}}}]);
